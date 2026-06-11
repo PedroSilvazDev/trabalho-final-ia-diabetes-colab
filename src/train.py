2966f166c1b8206991043e2427f33bddd340baf0
Pedro_Silva <160974160+PedroSilvazDev@users.noreply.github.com>
2026-06-10 22:02:27 -0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -25,6 +25,7 @@ class ModelResult:
 def train_knn(data: PreparedData) -> ModelResult:
     cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=RANDOM_STATE)
     param_grid = {"n_neighbors": list(range(3, 22, 2))}
+    # param_grid = {"n_neighbors": [9]}
 
     grid_search = GridSearchCV(
         KNeighborsClassifier(),
